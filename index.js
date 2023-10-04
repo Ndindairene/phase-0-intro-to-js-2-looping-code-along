9eef80147b28258c2b3dfebabf09a42844f2e9cd
Ndindairene <irene.ndindaa@gmail.com>
2023-10-04 10:42:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, event){
+    const thankYouMessage = [];
+    for(let i = 0; i < name.length; i++) {
+     thankYouMessage.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessage;
+}
+
+function countDown(number){
+ while(number>=0){
+ console.log(number)
+ number--;
+ }
+}
